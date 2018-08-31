42837f32bf99f78b24920c8d28e68eb471e2c42e
Peter <370167676@qq.com>
2018-08-31 16:07:27 +0800

    init commit

---
+++ web/src/main/java/ZeusApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Description:</p>
+ *
+ * @author minglu@toutoujinrong.com
+ * @date 8/30/2018 4:41 PM
+ */
+@SpringBootApplication
+public class ZeusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZeusApplication.class, args);
+  }
+}
---
+++ web/src/test/java/ApplicationTests.java
@@ -0,0 +1,23 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+/**
+ * <p>Description:</p>
+ *
+ * @author minglu@toutoujinrong.com
+ * @date 8/31/2018 10:03 AM
+ */
+@SpringBootTest
+public class ApplicationTests {
+
+  @Autowired
+  private JavaMailSender mailSender;
+
+  public void sendMailTest() {
+
+    SimpleMailMessage message = new SimpleMailMessage();
+
+  }
+}
